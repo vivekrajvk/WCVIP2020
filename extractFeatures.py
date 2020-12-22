dced9e1c230676a0caf8f42b6981dd8a24dcc485
vivekrajvk <69667366+vivekrajvk@users.noreply.github.com>
2020-12-22 20:35:14 +0530

    Add files via upload
    DVS demo files added to repo.

---
+++ extractFeatures.py
@@ -0,0 +1,41 @@
+import numpy as np
+import cv2
+import glob
+import os
+import re
+def extract_features(image_dir_name):
+    images = [cv2.imread(file) for file in sorted(glob.glob(os.path.join(image_dir_name, "*.jpg")), key=stringSplitByNumbers)]
+    BINS_NUMBER_PER_CHANNEL = 32
+    features = np.zeros((images.__len__(), BINS_NUMBER_PER_CHANNEL * 3), dtype=float)
+    for i in range(len(images)):
+        if i % 50 == 0:
+            print("Finished extracting features for %d frames" %i)
+        #
+
+        features[i,:] = extract_Histogram(images[i])
+    return features
+
+
+def stringSplitByNumbers(x):
+    r = re.compile('(\d+)')
+    l = r.split(x)
+    return [int(y) if y.isdigit() else y for y in l]
+
+def extract_Histogram(images):
+    BINS_NUMBER_PER_CHANNEL = 32
+    features = np.zeros((1, BINS_NUMBER_PER_CHANNEL * 3), dtype=float)
+    
+    r_values = images[:,:,0].flatten()
+    g_values= images[:, :, 1].flatten()
+    b_values = images[:, :, 2].flatten()
+
+    r_hist, _ = np.histogram(r_values, BINS_NUMBER_PER_CHANNEL, [0, 256])
+    normalized_r_hist =  r_hist / np.sum(r_hist)
+    g_hist, _ = np.histogram(g_values, BINS_NUMBER_PER_CHANNEL, [0, 256])
+    normalized_g_hist =  g_hist / np.sum(g_hist)
+    b_hist, _ = np.histogram(b_values, BINS_NUMBER_PER_CHANNEL, [0, 256])
+    normalized_b_hist =  b_hist / np.sum(b_hist)
+
+    features= np.concatenate((normalized_r_hist, normalized_g_hist, normalized_b_hist))
+    
+    return features
